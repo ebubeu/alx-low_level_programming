d4ac56b9f9ebc38e79bfc42bdeff34722a3847b6
ebubeu <uwahebube@gmail.com>
2023-03-18 02:06:32 -0700

    Update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	k = 1;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = k; j <= 9; j++)
+		{
+			putchar(i + 48);
+			putchar(j + 48);
+
+			if (i == 8 && j == 9)
+			{
+				putchar(' ');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		k++;
+	}
+	return (0);
+}
